7cf60fc4c1d863c6051df1a86998cf8fe95e37b5
vishalpalshanupatel <117573377+vishalpalshanupatel@users.noreply.github.com>
2024-11-05 23:15:24 +0530

    Update Nodes.jsx

--- Frontend/src/assets/components/Nodes.jsx
+++ Frontend/src/assets/components/Nodes.jsx
@@ -1,34 +1,41 @@
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
 function Nodes({ setAdjList }) { 
+   
     const [nodeCount, setNodeCount] = useState(1);
+   
     const [nodeConnections, setNodeConnections] = useState({});
 
+    // Handle input for each node's connections
     const handleConnectionsInput = (e) => {
         const { name, value } = e.target;
+        
         setNodeConnections({ ...nodeConnections, [name]: value });
     };
 
     const createAdjList = () => {
         let list = {};
+        // Initialize an empty array for each node
         for (let i = 1; i <= nodeCount; i++) {
             list[i] = [];
         }
 
+        // Populate adjacency list with connections
         for (const node in nodeConnections) {
             const connections = nodeConnections[node]
-                .split(',')
-                .map((connection) => parseInt(connection.trim()));
+                .split(',') 
+                .map((connection) => parseInt(connection.trim())); // Parse each connection as an integer
 
             list[node] = connections;
         }
 
+        // Update the adjacency list in parent component
         setAdjList(list);  
     };
 
     return (
         <div className="nodes_number">
             <label>Number of Nodes In the graph</label>
+           
             <input
                 value={nodeCount}
                 type="number"
@@ -38,6 +45,7 @@ function Nodes({ setAdjList }) {
             />
             <h3>Graph Input</h3>
 
+            // Render input fields for each node's connections 
             {Array.from({ length: nodeCount }, (_, index) => (
                 <div key={index + 1}>
                     <label>Node {index + 1} connections: </label>
@@ -53,6 +61,7 @@ function Nodes({ setAdjList }) {
             <button onClick={createAdjList}>Create Graph</button>
 
             <h4>Adjacency List</h4>
+             Display the current connections entered by the user 
             <pre>{JSON.stringify(nodeConnections, null, 2)}</pre>
         </div>
     );
