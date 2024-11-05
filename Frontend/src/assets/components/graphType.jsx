202c55913058f4f994b1efad9b1cd44ae1acb949
vishalpalshanupatel <117573377+vishalpalshanupatel@users.noreply.github.com>
2024-11-05 23:17:27 +0530

    Update graphType.jsx

--- Frontend/src/assets/components/graphType.jsx
+++ Frontend/src/assets/components/graphType.jsx
@@ -1,10 +1,13 @@
+
 import React, { useState } from 'react';
 
 function GraphType({ graphType, setGraphType }) {  
     return (
         <div className="graph_type">
             <label>Select Graph Type</label>
+            // Dropdown for selecting graph type 
             <select value={graphType} onChange={(e) => setGraphType(e.target.value)}>
+                // Options for directed and undirected graph types 
                 <option value="Directed">Directed</option>
                 <option value="unDirected">Un-Directed</option>
             </select>
