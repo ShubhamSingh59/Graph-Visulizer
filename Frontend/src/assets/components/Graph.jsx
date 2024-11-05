3ca87aa404bc36a730e119e0d978c4786a1bed0b
vishalpalshanupatel <117573377+vishalpalshanupatel@users.noreply.github.com>
2024-11-05 23:00:14 +0530

    Update Graph.jsx

--- Frontend/src/assets/components/Graph.jsx
+++ Frontend/src/assets/components/Graph.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GraphVisualizer from './GraphVisualizer'; 
-
+// Graph component definition
 function Graph({ adjList, graphType}) {
     return (
         <div>
+          //  Render GraphVisualizer and pass adjList and graphType as props
             <GraphVisualizer adjList={adjList} graphType={graphType}/>
         </div>
     );
